528de10e1ae00f743312c9646617ca9a965c4717
ngkerong <55265526+ngkerong@users.noreply.github.com>
2020-03-12 16:11:00 +0800

    Add files via upload

---
+++ Income.java
@@ -0,0 +1,16 @@
+/*Name: NG KE RONG*/
+/*No.Matrik:271063*/
+
+import java.util.*;
+
+public class Income {
+   
+   static Scanner sc = new Scanner (System.in); 
+   
+   public sattic void main(String []args){
+   
+       
+   }
+   
+   
+}  
